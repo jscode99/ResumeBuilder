4c6f364a0fc63bb1b8c70582f9abd5e0da54a4e4
Jishnu <jscode99@gmail.com>
2020-12-18 13:26:03 +0530

    Last commit done

---
+++ resumebuilder/src/utils/fake_data.js
@@ -0,0 +1,50 @@
+//Data used when the user hits "Example" btn
+const data = {
+  header: {
+    name: "Jishnu Satheesh",
+    address: "Lekshmi Bhavan ",
+    city: "Harpad",
+    state: "Kerala",
+    zip: "690512",
+    phone: "6282997803",
+    email: "jishnusatheesh27@gmai.com",
+    summary:
+      "The creator, The developer",
+  },
+  professional: {
+    company1: "Edith Industries",
+    local1: "Cochin",
+    position1: "Full stack javascript developer part-time",
+    start1: "Jan/2019",
+    end1: "Present",
+    desc1: [
+      "Responsible for all the material handled by blablabla.",
+      "Help others on their daily tasks.",
+    ],
+    company2: "Aero Developers",
+    local2: "Cochin",
+    position2: "Intern",
+    start2: "Jul/2017",
+    end2: "Dec/2018",
+    desc2: [
+      "Assist CEO on daily tasks.",
+      "Provide assistance to all employees.",
+    ],
+  },
+  education: {
+    institution: "Kerala University",
+    city: "Cochin",
+    major: "Bachelor of technology",
+    gradYear: "Present",
+    additional: "Blah Blah Blah",
+  },
+  additional: [
+    "5+ years of experience with Microsoft Office",
+    "Nodejs Developer",
+    "Adaptability",
+    "Interpersonal Communication",
+    "Friend of all",
+  ],
+};
+
+export default data;
